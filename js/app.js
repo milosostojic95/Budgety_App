433347f251f9daea94e70da2f12d1187c3ce9081
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-05-13 11:10:21 +0200

    Readed input date

--- js/app.js
+++ js/app.js
@@ -2,10 +2,45 @@ const budgetController = (function() {
 
 })();
 
-const UIControllet = (function() {
+const UIController = (function() {
+  const DOMStrings = {
+    inputType: '.add-type',
+    inputDescription: '.add-description',
+    inputValue: '.add-value',
+    inputBtn: '.add-btn'
+  }
 
+  return {
+    getInput: function() {
+      return {
+        addType: document.querySelector(DOMStrings.inputType).value,
+        addDescription: document.querySelector(DOMStrings.inputDescription).value,
+        addValue: document.querySelector(DOMStrings.inputValue).value,
+      }
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  }
 })();
 
-const controller = (function() {
+const controller = (function(budgetCtrl,UICtrl) {
+  // variables
+  const DOM = UICtrl.getDOMStrings();
+  const addItemBtn = document.querySelector(DOM.inputBtn);
 
-})();
+  //event
+  addItemBtn.addEventListener('click',ctrlAddItems);
+  addItemBtn.addEventListener('keypress', (event) => {
+    if( event.keyCode === 13 ) {
+      ctrlAddItems();
+    }
+  })
+
+  // functions
+  function ctrlAddItems() {
+    // 1. get input date
+    const input = UICtrl.getInput();
+    console.log(input)
+  }
+})(budgetController,UIController);
